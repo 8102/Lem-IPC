9395025dca25acbb5bf99246bcc9edfccb41ecf1
Hugo Prenat <hugo.prenat@epitech.eu>
2015-03-08 13:17:26 +0100

    This is 'la base' for our ia just got to improve where it got to go

--- src/move.c
+++ src/move.c
@@ -5,22 +5,87 @@
 ** Login   <prenat_h@epitech.eu>
 **
 ** Started on  Thu Mar  5 18:51:39 2015 Hugo Prenat
-** Last update Sat Mar  7 17:39:44 2015 Hugo Prenat
+** Last update Sun Mar  8 13:12:57 2015 Hugo Prenat
 */
 
 #include "lemiPC.h"
 
-/* void	go_to_enemy(t_player *player) */
-/* { */
-/*   int	x_enemy; */
-/*   int	y_enemy; */
-/*   x_enemy = 0; */
-/*   y_enemy = 0; */
-/*   if (player->y > y_enemy) */
-/*     player->y += 1; */
-/*   else if */
-/*     player->y -= 1; */
-/* } */
+int	nearest_on_x(t_player *player, int d, unsigned char *map, int *y)
+{
+  int	i;
+  int	x;
+
+  i = 0;
+  while (i < d)
+    {
+      if (map[POS(player->x - d + i, player->y - i)] != 0
+	  && map[POS(player->x - d + i, player->y - i)] != player->team)
+	{
+	  x = player->x - d + 1;
+	  *y = player->y - i;
+	}
+      else if (map[POS(player->x + d - i, player->y + i)] != 0
+	       && map[POS(player->x + d - i, player->y + i)] != player->team)
+	{
+	  x = player->x + d - 1;
+	  *y = player->y + i;
+	}
+      i++;
+    }
+  printf("x === %d\n", x);
+  return (x);
+}
+
+int	nearest_on_y(t_player *player, int d, unsigned char *map, int *x)
+{
+  int	i;
+  int	y;
+
+  i = 0;
+  while (i < d)
+    {
+      if (map[POS(player->x - i, player->y + d - i)] != 0
+	  && map[POS(player->x - i, player->y + d - i)] != player->team)
+	{
+	  *x = player->x - 1;
+	  y = player->y + d - i;
+	}
+      else if (map[POS(player->x + d - i, player->y - i)] != 0
+	       && map[POS(player->x + d - i, player->y - i)] != player->team)
+	{
+	  *x = player->x + d - 1;
+	  y = player->y - i;
+	}
+      i++;
+    }
+  printf("y === %d\n", y);
+  return (y);
+}
+
+int	find_enemy(t_player *player, unsigned char *map)
+{
+  int	i;
+  int	enemy_pos;
+
+  enemy_pos = -1;
+  i = 0;
+  while (i < MAP_SIZE)
+    {
+      if (map[i] != 0 && map[i] != player->team)
+	enemy_pos = i;
+      i++;
+    }
+  return (enemy_pos);
+}
+
+void	go_to_enemy(t_player *player, unsigned char *map, int *x, int *y)
+{
+  int	enemy_pos;
+
+  enemy_pos = find_enemy(player, map);
+  nearest_on_x(player, map, x, y);
+  nearest_on_y(player, map, x, y);
+}
 
 void	update_player_pos(int x, int y, t_player *player, unsigned char *map)
 {
@@ -39,12 +104,16 @@ void	update_player_pos(int x, int y, t_player *player, unsigned char *map)
 
 void	move(t_player *player, unsigned char *map)
 {
+  int	x;
+  int	y;
+
   while (is_alive(player, map) == 0)
     {
       change_state_sem(player, -1);
-      update_player_pos(player->x + rand() % 2, player->y + rand() % 2,
-			player, map);
+      go_to_enemy(player, map, &x, &y);
+      update_player_pos(x, y, player, map);
       change_state_sem(player, 1);
-      usleep(100000);
+      sleep(1);
+      /* usleep(100000); */
     }
 }
